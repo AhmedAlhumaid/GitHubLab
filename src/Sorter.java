a50eab0cfb864a53d32e84294c9245cbb4286dec
Ahmed Alhumaid <humaidakah@gmail.com>
2024-02-05 14:57:46 +0300

    selection sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,22 @@
 public class Sorter {
     public static void main(String[] args) {
 
+
+    }
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            // Find the index of the minimum element in the unsorted part of the array
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element of the unsorted part
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
